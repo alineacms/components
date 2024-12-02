ffc1c5f1cf5762c0d9ce796cdd007df8d7946a1b
Ben Merckx <ben@codeurs.be>
2024-12-02 17:10:26 +0100

    Interim

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,8 +8,16 @@ export interface ButtonProps extends ButtonPrimitiveProps {
   intent?: 'primary' | 'secondary' | 'danger' | 'warning'
   size?: 'medium' | 'large' | 'square' | 'extra-small' | 'small'
   shape?: 'circle' | 'plain' | 'default'
+  appearance?: 'solid' | 'outline' | 'plain'
 }
 
-export function Button({intent, size, ...props}: ButtonProps) {
-  return <ButtonPrimitive data-intent={intent} data-size={size} {...props} />
+export function Button({intent, size, appearance, ...props}: ButtonProps) {
+  return (
+    <ButtonPrimitive
+      data-intent={intent}
+      data-size={size}
+      data-appearance={appearance}
+      {...props}
+    />
+  )
 }
---
+++ src/components/Tree.tsx
@@ -0,0 +1,64 @@
+import type {
+  TreeItemProps as TreeItemPrimitiveProps,
+  TreeProps
+} from 'react-aria-components'
+import {
+  UNSTABLE_ListLayout as ListLayout,
+  UNSTABLE_TreeItemContent as TreeItemContentPrimitive,
+  UNSTABLE_TreeItem as TreeItemPrimitive,
+  UNSTABLE_Tree as TreePrimitive,
+  UNSTABLE_Virtualizer as Virtualizer
+} from 'react-aria-components'
+import './Tree.css'
+import {type ComponentProps, useMemo} from 'react'
+import {Button} from './Button'
+import {Checkbox} from './Checkbox'
+
+export const Tree = <T extends object>({className, ...props}: TreeProps<T>) => {
+  const layout = useMemo(() => {
+    return new ListLayout({
+      rowHeight: 30
+    })
+  }, [])
+
+  return (
+    <Virtualizer layout={layout}>
+      <TreePrimitive {...props}>{props.children}</TreePrimitive>
+    </Virtualizer>
+  )
+}
+
+export const TreeItem = <T extends object>({
+  className,
+  ...props
+}: TreeItemPrimitiveProps<T>) => {
+  return <TreeItemPrimitive {...props}>{props.children}</TreeItemPrimitive>
+}
+
+export const TreeItemContent = (
+  props: ComponentProps<typeof TreeItemContentPrimitive>
+) => {
+  return (
+    <TreeItemContentPrimitive {...props}>
+      {props.children}
+    </TreeItemContentPrimitive>
+  )
+}
+
+export const TreeItemIndicator = () => {
+  return (
+    <Button slot="chevron" size="extra-small">
+      indicator
+    </Button>
+  )
+}
+
+export const TreeItemCheckbox = () => {
+  return <Checkbox slot="selection" />
+}
+
+export const TreeItemLabel = (
+  props: React.HtmlHTMLAttributes<HTMLSpanElement>
+) => {
+  return <span {...props} />
+}
