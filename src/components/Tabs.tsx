f58dc638b18e77fcb6107f43c82488484552f5b4
Stijn Van Doorslaer <stijn@codeurs.be>
2024-12-11 18:21:40 +0100

    Tabs layout

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,11 @@
+import {Tabs as RACTabs} from 'react-aria-components'
+import type {TabsProps as RACTabsProps} from 'react-aria-components'
+import './Tabs.css'
+
+export interface TabsProps extends RACTabsProps {
+  variant?: 'line' | 'subtle' | 'enclosed'
+}
+
+export function Tabs({variant, ...props}: TabsProps) {
+  return <RACTabs data-variant={variant} {...props} />
+}
--- stories/Tabs.stories.tsx
+++ stories/Tabs.stories.tsx
@@ -1,15 +1,43 @@
 import {Tab, TabList, TabPanel} from 'react-aria-components'
-import {Tabs} from '../src/todo/Tabs.tsx'
+import {Tabs} from '../src/components/Tabs.tsx'
 
 export const Example = (args: any) => (
-  <Tabs {...args}>
-    <TabList aria-label="History of Ancient Rome">
-      <Tab id="FoR">Founding of Rome</Tab>
-      <Tab id="MaR">Monarchy and Republic</Tab>
-      <Tab id="Emp">Empire</Tab>
-    </TabList>
-    <TabPanel id="FoR">Arma virumque cano, Troiae qui primus ab oris.</TabPanel>
-    <TabPanel id="MaR">Senatus Populusque Romanus.</TabPanel>
-    <TabPanel id="Emp">Alea jacta est.</TabPanel>
-  </Tabs>
+  <>
+    <Tabs aria-label="History of Ancient Rome" {...args}>
+      <TabList>
+        <Tab id="FoR">Founding of Rome</Tab>
+        <Tab id="MaR">Monarchy and Republic</Tab>
+        <Tab id="Emp">Empire</Tab>
+      </TabList>
+      <TabPanel id="FoR">
+        Arma virumque cano, Troiae qui primus ab oris.
+      </TabPanel>
+      <TabPanel id="MaR">Senatus Populusque Romanus.</TabPanel>
+      <TabPanel id="Emp">Alea jacta est.</TabPanel>
+    </Tabs>
+    <Tabs variant="subtle" aria-label="History of Ancient Rome" {...args}>
+      <TabList>
+        <Tab id="FoR">Founding of Rome</Tab>
+        <Tab id="MaR">Monarchy and Republic</Tab>
+        <Tab id="Emp">Empire</Tab>
+      </TabList>
+      <TabPanel id="FoR">
+        Arma virumque cano, Troiae qui primus ab oris.
+      </TabPanel>
+      <TabPanel id="MaR">Senatus Populusque Romanus.</TabPanel>
+      <TabPanel id="Emp">Alea jacta est.</TabPanel>
+    </Tabs>
+    <Tabs variant="enclosed" aria-label="History of Ancient Rome" {...args}>
+      <TabList>
+        <Tab id="FoR">Founding of Rome</Tab>
+        <Tab id="MaR">Monarchy and Republic</Tab>
+        <Tab id="Emp">Empire</Tab>
+      </TabList>
+      <TabPanel id="FoR">
+        Arma virumque cano, Troiae qui primus ab oris.
+      </TabPanel>
+      <TabPanel id="MaR">Senatus Populusque Romanus.</TabPanel>
+      <TabPanel id="Emp">Alea jacta est.</TabPanel>
+    </Tabs>
+  </>
 )
