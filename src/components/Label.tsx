7c1756aba2da86c6b74400c124a98fa31ca20a24
thibovi <thibovaningelgem@gmail.com>
2025-02-26 16:00:15 +0100

    Started label

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,20 +1,37 @@
-import clsx from 'clsx'
 import {
   Label as LabelPrimitive,
   type LabelProps as LabelPrimitiveProps
 } from 'react-aria-components'
 
 import './Label.css'
+import type {ReactNode} from 'react'
 
 interface LabelProps extends LabelPrimitiveProps {
   isRequired?: boolean
+  label: ReactNode
+  description?: ReactNode
+  errorMessage?: ReactNode
 }
 
-export function Label({isRequired, ...props}: LabelProps) {
-  if (!props.children) return null
+export function Label({
+  label,
+  description,
+  errorMessage,
+  isRequired,
+  ...props
+}: LabelProps) {
   return (
-    <LabelPrimitive {...props} className={clsx('alinea-rac-Label')}>
-      {props.children + (isRequired ? ' *' : '')}
-    </LabelPrimitive>
+    <>
+      <div className="alinea-rac-Label">
+        <div className="alinea-rac-Label-header">
+          <LabelPrimitive {...props} className="alinea-rac-Label-label">
+            {label}
+          </LabelPrimitive>
+          <div className="alinea-rac-Label-description">{description}</div>
+        </div>
+        {props.children}
+        <div className="alinea-rac-Label-error">{errorMessage}</div>
+      </div>
+    </>
   )
 }
