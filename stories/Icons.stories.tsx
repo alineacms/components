834a72ac1d4a7d7034269c382189d5d31aad93d4
thibovi <thibovaningelgem@gmail.com>
2025-02-28 16:08:05 +0100

    Added Icons

--- stories/Icons.stories.tsx
+++ stories/Icons.stories.tsx
@@ -1,12 +1,31 @@
-import {IcRoundKeyboardArrowDown} from '../src/icons/IcRoundKeyboardArrowDown.tsx'
-import {IcRoundKeyboardArrowUp} from '../src/icons/IcRoundKeyboardArrowUp.tsx'
-import {IcRoundKeyboardArrowLeft} from '../src/icons/IcRoundKeyboardArrowLeft.tsx'
-import {IcRoundKeyboardArrowRight} from '../src/icons/IcRoundKeyboardArrowRight.tsx'
-import {IcDocument} from '../src/icons/IcDocument.tsx'
-import { Stack } from './Stack.tsx'
+import { IcRoundKeyboardArrowDown } from '../src/icons/IcRoundKeyboardArrowDown.tsx';
+import { IcRoundKeyboardArrowUp } from '../src/icons/IcRoundKeyboardArrowUp.tsx';
+import { IcRoundKeyboardArrowLeft } from '../src/icons/IcRoundKeyboardArrowLeft.tsx';
+import { IcRoundKeyboardArrowRight } from '../src/icons/IcRoundKeyboardArrowRight.tsx';
+import { IcDocument } from '../src/icons/IcRoundDocument.tsx';
+import { IcRoundShare } from '../src/icons/IcRoundShare.tsx';
+import { IcRoundRemoveRedEye } from '../src/icons/IcRoundRemoveRedEye.tsx';
+import { IcRoundMoreVert } from '../src/icons/IcRoundMoreVert.tsx';
+import { IcRoundKeyboardTab } from '../src/icons/IcRoundKeyboardTab.tsx';
+import { IcRoundArrowBack } from '../src/icons/IcRoundArrowBack.tsx';
+import { IcRoundArrowForward } from '../src/icons/IcRoundArrowForward.tsx';
+import { IcRoundRefresh } from '../src/icons/IcRoundRefresh.tsx';
+import { IcRoundOpenInNew } from '../src/icons/IcRoundOpenInNew.tsx';
+import { IcRoundLanguage } from '../src/icons/IcRoundLanguage.tsx';
+import { IcRoundArticle } from '../src/icons/IcRoundArticle.tsx';
+import { IcRoundContentCopy } from '../src/icons/IcRoundContentCopy.tsx';
+import { IcRoundClose } from '../src/icons/IcRoundClose.tsx';
+import { IcRoundCloudUpload } from '../src/icons/IcRoundCloudUpload.tsx';
+import { IcRoundAccountCircle } from '../src/icons/IcRoundAccountCircle.tsx';
+import { IcRoundBrightness } from '../src/icons/IcRoundBrightness.tsx';
+import { IcRoundTextFields } from '../src/icons/IcRoundTextFields.tsx';
+import { IcRoundSearch } from '../src/icons/IcRoundSearch.tsx';
+import { IcRoundSettings } from '../src/icons/IcRoundSettings.tsx';
+import { Stack } from './Stack.tsx';
+
 
 export const Example = (args: any) => (
-  <>
+  <div style={{ columns: 2 }}>
     <Stack direction='row' align='center'>
       <IcRoundKeyboardArrowDown />
       <p>IcRoundKeyboardArrowDown</p>
@@ -15,7 +34,7 @@ export const Example = (args: any) => (
       <IcRoundKeyboardArrowUp />
       <p>IcRoundKeyboardArrowUp</p>
     </Stack>
-      <Stack direction='row' align='center'>
+    <Stack direction='row' align='center'>
       <IcRoundKeyboardArrowLeft />
       <p>IcRoundKeyboardArrowLeft</p>
     </Stack>
@@ -27,7 +46,82 @@ export const Example = (args: any) => (
       <IcDocument />
       <p>IcDocument</p>
     </Stack>
-  </>
-  
+    <Stack direction='row' align='center'>
+      <IcRoundShare />
+      <p>IcRoundShare</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundRemoveRedEye />
+      <p>IcRoundRemoveRedEye</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundMoreVert />
+      <p>IcRoundMoreVert</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundKeyboardTab />
+      <p>IcRoundKeyboardTab</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundArrowBack />
+      <p>IcRoundArrowBack</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundArrowForward />
+      <p>IcRoundArrowForward</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundRefresh />
+      <p>IcRoundRefresh</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundOpenInNew />
+      <p>IcRoundOpenInNew</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundLanguage />
+      <p>IcRoundLanguage</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundArticle />
+      <p>IcRoundArticle</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundContentCopy />
+      <p>IcRoundContentCopy</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundClose />
+      <p>IcRoundClose</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundCloudUpload />
+      <p>IcRoundCloudUpload</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundAccountCircle />
+      <p>IcRoundAccountCircle</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundBrightness />
+      <p>IcRoundBrightness</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundTextFields />
+      <p>IcRoundTextFields</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundSearch />
+      <p>IcRoundSearch</p>
+    </Stack>
+    <Stack direction='row' align='center'>
+      <IcRoundSettings />
+      <p>IcRoundSettings</p>
+    </Stack>
+    
+  </div>
 
-)
+);
+
+
+
