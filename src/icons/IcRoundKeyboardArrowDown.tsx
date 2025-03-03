620bcab1cf90f99f8858a27368da327eb4c09f7c
thibovi <thibovaningelgem@gmail.com>
2025-03-03 12:19:03 +0100

    Changed icon currentcolor to var color

--- src/icons/IcRoundKeyboardArrowDown.tsx
+++ src/icons/IcRoundKeyboardArrowDown.tsx
@@ -11,7 +11,7 @@ export function IcRoundKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        fill="currentColor"
+        fill="var(--icon-color-plain)"
         d="M8.12 9.29L12 13.17l3.88-3.88a.996.996 0 1 1 1.41 1.41l-4.59 4.59a.996.996 0 0 1-1.41 0L6.7 10.7a.996.996 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0"
       />
     </svg>
