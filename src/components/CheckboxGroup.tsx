77c988558d96013c563c9dbfd4456467987f810e
thibovi <thibovaningelgem@gmail.com>
2025-03-04 16:29:40 +0100

    Refactor CheckboxGroup component: enhance error handling, improve structure, use label, and add description support

--- src/components/CheckboxGroup.tsx
+++ src/components/CheckboxGroup.tsx
@@ -2,15 +2,30 @@ import {
   CheckboxGroup as CheckboxGroupPrimitive,
   type CheckboxGroupProps as CheckboxPrimitiveGroupProps,
   FieldError,
-  Label,
-  Text,
   type ValidationResult
 } from 'react-aria-components'
 
+import {
+  Checkbox as CheckboxPrimitive,
+  type CheckboxProps
+} from 'react-aria-components'
 import './CheckboxGroup.css'
 import clsx from 'clsx'
+import {Label} from './Label.tsx' 
 
-export interface CheckboxGroupProps extends CheckboxPrimitiveGroupProps {
+export type {CheckboxProps} from 'react-aria-components'
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxPrimitive
+      {...props}
+      className={clsx('alinea-rac-Checkbox', props.className)}
+    />
+  )
+}
+
+export interface CheckboxGroupProps
+  extends Omit<CheckboxPrimitiveGroupProps, 'children'> {
   children?: React.ReactNode
   label?: string
   description?: string
@@ -29,10 +44,40 @@ export function CheckboxGroup({
       {...props}
       className={clsx('alinea-rac-CheckboxGroup', props.className)}
     >
-      {label && <Label>{label}</Label>}
-      {children}
-      {description && <Text slot="description">{description}</Text>}
-      <FieldError className="alinea-rac-FieldError">{errorMessage}</FieldError>
+      {label && (
+        <Label
+          label={label}
+          description={description}
+          errorMessage={errorMessage}
+        >
+          {children}
+        </Label>
+      )}
+      {!label && children}
+
+      {errorMessage && (
+        <FieldError className="alinea-rac-FieldError">
+          {typeof errorMessage === 'function'
+            ? errorMessage({
+                isInvalid: true,
+                validationErrors: [],
+                validationDetails: {
+                  badInput: false,
+                  customError: false,
+                  patternMismatch: false,
+                  rangeOverflow: false,
+                  rangeUnderflow: false,
+                  stepMismatch: false,
+                  tooLong: false,
+                  tooShort: false,
+                  typeMismatch: false,
+                  valid: false,
+                  valueMissing: false
+                }
+              })
+            : errorMessage}
+        </FieldError>
+      )}
     </CheckboxGroupPrimitive>
   )
 }
--- stories/CheckboxGroup.stories.tsx
+++ stories/CheckboxGroup.stories.tsx
@@ -1,14 +1,35 @@
 import {Checkbox} from '../src/components/Checkbox.tsx'
 import {CheckboxGroup} from '../src/components/CheckboxGroup.tsx'
-
+import {Stack} from './Stack.tsx'
 export const Example = (args: any) => (
-  <CheckboxGroup {...args}>
-    <Checkbox value="soccer">Soccer</Checkbox>
-    <Checkbox value="baseball">Baseball</Checkbox>
-    <Checkbox value="basketball">Basketball</Checkbox>
-  </CheckboxGroup>
+  <Stack>
+    <CheckboxGroup {...args}>
+      <Checkbox value="soccer">Soccer</Checkbox>
+      <Checkbox value="baseball">Baseball</Checkbox>
+      <Checkbox value="basketball">Basketball</Checkbox>
+    </CheckboxGroup>
+
+    <CheckboxGroup
+      label="Invalid selection"
+      errorMessage="You must select at least one option"
+    >
+      <Checkbox value="option1">Option 1</Checkbox>
+      <Checkbox value="option2">Option 2</Checkbox>
+      <Checkbox value="option3">Option 3</Checkbox>
+    </CheckboxGroup>
+
+    <CheckboxGroup
+      label="More choices"
+      description="Choose as many as you like"
+    >
+      <Checkbox value="optionA">Option A</Checkbox>
+      <Checkbox value="optionB">Option B</Checkbox>
+      <Checkbox value="optionC">Option C</Checkbox>
+    </CheckboxGroup>
+  </Stack>
 )
 
 Example.args = {
-  label: 'Favorite sports'
+  label: 'Favorite sports',
+  description: 'Select all that apply'
 }
