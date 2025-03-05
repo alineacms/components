44f62552ef698b2f8bc779d05bb792d56f201ec4
Ben Merckx <ben@codeurs.be>
2025-03-05 12:47:41 +0100

    Revert "Started label"
    This reverts commit 7c1756aba2da86c6b74400c124a98fa31ca20a24.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,37 +1,20 @@
+import clsx from 'clsx'
 import {
   Label as LabelPrimitive,
   type LabelProps as LabelPrimitiveProps
 } from 'react-aria-components'
 
 import './Label.css'
-import type {ReactNode} from 'react'
 
 interface LabelProps extends LabelPrimitiveProps {
   isRequired?: boolean
-  label: ReactNode
-  description?: ReactNode
-  errorMessage?: ReactNode
 }
 
-export function Label({
-  label,
-  description,
-  errorMessage,
-  isRequired,
-  ...props
-}: LabelProps) {
+export function Label({isRequired, ...props}: LabelProps) {
+  if (!props.children) return null
   return (
-    <>
-      <div className="alinea-rac-Label">
-        <div className="alinea-rac-Label-header">
-          <LabelPrimitive {...props} className="alinea-rac-Label-label">
-            {label}
-          </LabelPrimitive>
-          <div className="alinea-rac-Label-description">{description}</div>
-        </div>
-        {props.children}
-        <div className="alinea-rac-Label-error">{errorMessage}</div>
-      </div>
-    </>
+    <LabelPrimitive {...props} className={clsx('alinea-rac-Label')}>
+      {props.children + (isRequired ? ' *' : '')}
+    </LabelPrimitive>
   )
 }
