5fab7dacab043e245c4adadd45a2e1ea62c5af1b
thibovi <thibovaningelgem@gmail.com>
2025-03-05 12:24:44 +0100

    Enhance Label component: support function for errorMessage and improve context handling

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,22 +1,23 @@
 import {
+  FieldError,
   Label as LabelPrimitive,
   type LabelProps as LabelPrimitiveProps
-} from 'react-aria-components';
+} from 'react-aria-components'
 
-import './Label.css';
-import type { ReactNode } from 'react';
-import { createContext, useContext } from 'react';
+import './Label.css'
+import type {ReactNode} from 'react'
+import {createContext, useContext} from 'react'
 
-const LabelContext = createContext<{ errorMessage?: ReactNode }>({});
+const LabelContext = createContext<{errorMessage?: ReactNode}>({})
 
 interface LabelProps extends LabelPrimitiveProps {
-  isRequired?: boolean;
-  label: ReactNode;
-  description?: ReactNode;
-  errorMessage?: ReactNode;
-  isDisabled?: boolean;
-  icon?: ReactNode;
-  children: ReactNode;
+  isRequired?: boolean
+  label: ReactNode
+  description?: ReactNode
+  errorMessage?: ReactNode | ((validation: {isInvalid: boolean}) => ReactNode)
+  isDisabled?: boolean
+  icon?: ReactNode
+  children: ReactNode
 }
 
 export function Label({
@@ -30,24 +31,43 @@ export function Label({
   ...props
 }: LabelProps) {
   return (
-    <LabelContext.Provider value={{ errorMessage }}>
+    <LabelContext.Provider
+      value={{
+        errorMessage:
+          typeof errorMessage === 'function'
+            ? errorMessage({isInvalid: true})
+            : errorMessage
+      }}
+    >
       <div className="alinea-rac-Label-container">
         <div className="alinea-rac-Label-header">
           <div className="alinea-rac-Label-title">
             {icon && <span className="alinea-rac-Label-icon">{icon}</span>}
             <LabelPrimitive {...props} className="alinea-rac-Label-label">
               {label}
+              {isRequired && (
+                <span className="alinea-rac-Label-required"> *</span>
+              )}
             </LabelPrimitive>
           </div>
-          {description && <div className="alinea-rac-Label-description">{description}</div>}
+          {description && (
+            <div className="alinea-rac-Label-description">{description}</div>
+          )}
         </div>
         {children}
-        {errorMessage && <div className="alinea-rac-Label-error">{errorMessage}</div>}
+
+        {errorMessage && (
+          <FieldError className="alinea-rac-Label-error">
+            {typeof errorMessage === 'function'
+              ? errorMessage({isInvalid: true})
+              : errorMessage}
+          </FieldError>
+        )}
       </div>
     </LabelContext.Provider>
-  );
+  )
 }
 
 export function useLabelContext() {
-  return useContext(LabelContext);
+  return useContext(LabelContext)
 }
