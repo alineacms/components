b34be3efc980bd5d720fe2d414880a410b220f70
Ben <ben@codeurs.be>
2025-03-07 12:57:12 +0100

    Update RadioGroup.stories.tsx

--- stories/RadioGroup.stories.tsx
+++ stories/RadioGroup.stories.tsx
@@ -9,16 +9,8 @@ export const Example = () => {
         <Radio value="baseball">Baseball</Radio>
         <Radio value="basketball">Basketball</Radio>
       </RadioGroup>
-      
-      <RadioGroup label="Favorite codeurs member" description="Who is your favorite codeurs member?"> 
-        <Radio value="ben">Ben</Radio>
-        <Radio value="stijn">Stijn</Radio>
-        <Radio value="brecht">Brecht</Radio>
-        <Radio value="dimi">Dimi</Radio>
-        <Radio value="david">David</Radio>
-      </RadioGroup>
 
-      <RadioGroup label="favorite juice" isDisabled description="What is your favorite juice?">
+      <RadioGroup label="Favorite juice" isDisabled description="What is your favorite juice?">
         <Radio value="apple">Apple</Radio>
         <Radio value="orange">Orange</Radio>
         <Radio value="grape">Grape</Radio>
