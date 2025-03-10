b1b2f4900f54a1557705e7bc87b713f9e071aa8e
thibovi <thibovaningelgem@gmail.com>
2025-03-10 10:17:06 +0100

    WIP

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -9,27 +9,18 @@ import {
   type ListBoxItemProps,
   Popover
 } from 'react-aria-components'
-import {Label} from './Label.tsx'
-
+import { Label, type LabelSharedProps, labelProps } from './Label.tsx'
 import './ComboBox.css'
 
 export interface ComboBoxProps<T extends object>
-  extends Omit<ComboboxPrimitiveProps<T>, 'children'> {
-  label?: string
-  description?: string | null
-  errorMessage?: string
+  extends Omit<ComboboxPrimitiveProps<T>, 'children'>,
+    LabelSharedProps {
   children: React.ReactNode | ((item: T) => React.ReactNode)
 }
 
-export function ComboBox<T extends object>({
-  label,
-  description,
-  errorMessage,
-  children,
-  ...props
-}: ComboBoxProps<T>) {
+export function ComboBox<T extends object>({ children, ...props }: ComboBoxProps<T>) {
   return (
-    <Label label={label} errorMessage={errorMessage} description={description}>
+    <Label {...labelProps(props)}>
       <ComboboxPrimitive
         {...props}
         className={clsx('alinea-rac-Combobox', props.className)}
