50e4d87820da1113f72f0fb5d2b9f5bda88d9a7b
thibovi <thibovaningelgem@gmail.com>
2025-03-11 11:56:44 +0100

    Refactor ComboBox component for improved styling, accessibility, and item rendering

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,42 +1,70 @@
 import clsx from 'clsx'
+import type {
+  ComboBoxProps as ComboBoxPrimitiveProps,
+  ListBoxItemProps
+} from 'react-aria-components'
 import {
   Button,
-  ComboBox as ComboboxPrimitive,
-  type ComboBoxProps as ComboboxPrimitiveProps,
+  ComboBox as ComboBoxPrimitive,
   Input,
   ListBox,
   ListBoxItem,
-  type ListBoxItemProps,
   Popover
 } from 'react-aria-components'
-import { Label, type LabelSharedProps, labelProps } from './Label.tsx'
+import {IcRoundCheck} from '../icons/IcRoundCheck.tsx'
+import {IcRoundKeyboardArrowDown} from '../icons/IcRoundKeyboardArrowDown.tsx'
+import {Label, type LabelSharedProps, labelProps} from './Label.tsx'
 import './ComboBox.css'
 
 export interface ComboBoxProps<T extends object>
-  extends Omit<ComboboxPrimitiveProps<T>, 'children'>,
+  extends Omit<ComboBoxPrimitiveProps<T>, 'children'>,
     LabelSharedProps {
+  items?: Iterable<T>
   children: React.ReactNode | ((item: T) => React.ReactNode)
 }
 
-export function ComboBox<T extends object>({ children, ...props }: ComboBoxProps<T>) {
+export function ComboBox<T extends object>({
+  children,
+  items,
+  ...props
+}: ComboBoxProps<T>) {
   return (
-    <Label {...labelProps(props)}>
-      <ComboboxPrimitive
-        {...props}
-        className={clsx('alinea-rac-Combobox', props.className)}
-      >
-        <div>
-          <Input />
-          <Button>▼</Button>
+    <ComboBoxPrimitive {...props}>
+      <Label {...labelProps(props)}>
+        <div className={clsx('alinea-rac-Combobox', props.className)}>
+          <Input className="alinea-rac-Combobox-input" />
+          <Button className="alinea-rac-Combobox-button">
+            <IcRoundKeyboardArrowDown />
+          </Button>
         </div>
-        <Popover>
-          <ListBox>{children}</ListBox>
+        <Popover className="alinea-rac-Combobox-popover">
+          <ListBox
+            items={items}
+            className="alinea-rac-Combobox-popover-listbox"
+          >
+            {children}
+          </ListBox>
         </Popover>
-      </ComboboxPrimitive>
-    </Label>
+      </Label>
+    </ComboBoxPrimitive>
   )
 }
 
-export function ComboBoxItem(props: ListBoxItemProps) {
-  return <ListBoxItem {...props} />
+export function ComboBoxItem({children, ...props}: ListBoxItemProps) {
+  return (
+    <ListBoxItem
+      className="alinea-rac-ComboBoxItem"
+      textValue={typeof children === 'string' ? children : props.textValue}
+      {...props}
+    >
+      {({isSelected}) => (
+        <>
+          {children}
+          {isSelected && (
+            <IcRoundCheck className="alinea-rac-ComboBoxItem-check" />
+          )}
+        </>
+      )}
+    </ListBoxItem>
+  )
 }
--- stories/ComboBox.stories.tsx
+++ stories/ComboBox.stories.tsx
@@ -6,6 +6,66 @@ export const Example = (args: any) => (
     <ComboBoxItem>Mint</ComboBoxItem>
     <ComboBoxItem>Strawberry</ComboBoxItem>
     <ComboBoxItem>Vanilla</ComboBoxItem>
+    <ComboBoxItem>Rocky Road</ComboBoxItem>
+    <ComboBoxItem>Bubblegum</ComboBoxItem>
+    <ComboBoxItem>Neapolitan</ComboBoxItem>
+    <ComboBoxItem>Cookie Dough</ComboBoxItem>
+    <ComboBoxItem>Green Tea</ComboBoxItem>
+    <ComboBoxItem>Spicy Tuna</ComboBoxItem>
+    <ComboBoxItem>Pickle Juice</ComboBoxItem>
+    <ComboBoxItem>Wasabi Pea</ComboBoxItem>
+    <ComboBoxItem>Kimchi</ComboBoxItem>
+    <ComboBoxItem>Ghost Pepper</ComboBoxItem>
+    <ComboBoxItem>Salted Caramel Bacon</ComboBoxItem>
+    <ComboBoxItem>Garlic Bread</ComboBoxItem>
+    <ComboBoxItem>Fried Chicken</ComboBoxItem>
+    <ComboBoxItem>Buffalo Wing</ComboBoxItem>
+    <ComboBoxItem>Jalapeno Popper</ComboBoxItem>
+    <ComboBoxItem>Watermelon</ComboBoxItem>
+    <ComboBoxItem>Apple</ComboBoxItem>
+    <ComboBoxItem>Banana</ComboBoxItem>
+    <ComboBoxItem>Caramel</ComboBoxItem>
+    <ComboBoxItem>Butterscotch</ComboBoxItem>
+    <ComboBoxItem>Maple</ComboBoxItem>
+    <ComboBoxItem>Peppermint</ComboBoxItem>
+    <ComboBoxItem>Pistachio</ComboBoxItem>
+    <ComboBoxItem>Walnut</ComboBoxItem>
+    <ComboBoxItem>Pineapple</ComboBoxItem>
+    <ComboBoxItem>Almond</ComboBoxItem>
+    <ComboBoxItem>Coffee</ComboBoxItem>
+    <ComboBoxItem>Espresso</ComboBoxItem>
+    <ComboBoxItem>Chai</ComboBoxItem>
+    <ComboBoxItem>Matcha</ComboBoxItem>
+    <ComboBoxItem>Red Bean</ComboBoxItem>
+    <ComboBoxItem>Yam</ComboBoxItem>
+    <ComboBoxItem>Avocado</ComboBoxItem>
+    <ComboBoxItem>Durian</ComboBoxItem>
+    <ComboBoxItem>Lychee</ComboBoxItem>
+    <ComboBoxItem>Mango</ComboBoxItem>
+    <ComboBoxItem>Papaya</ComboBoxItem>
+    <ComboBoxItem>Jackfruit</ComboBoxItem>
+    <ComboBoxItem>Guava</ComboBoxItem>
+    <ComboBoxItem>Starfruit</ComboBoxItem>
+    <ComboBoxItem>Dragonfruit</ComboBoxItem>
+    <ComboBoxItem>Gooseberry</ComboBoxItem>
+    <ComboBoxItem>Black Currant</ComboBoxItem>
+    <ComboBoxItem>Cranberry</ComboBoxItem>
+    <ComboBoxItem>Plum</ComboBoxItem>
+    <ComboBoxItem>Prune</ComboBoxItem>
+    <ComboBoxItem>Raisin</ComboBoxItem>
+    <ComboBoxItem>Fig</ComboBoxItem>
+    <ComboBoxItem>Apricot</ComboBoxItem>
+    <ComboBoxItem>Nectarine</ComboBoxItem>
+    <ComboBoxItem>Wasabi</ComboBoxItem>
+    <ComboBoxItem>Garlic</ComboBoxItem>
+    <ComboBoxItem>Cheese</ComboBoxItem>
+    <ComboBoxItem>Bacon</ComboBoxItem>
+    <ComboBoxItem>Squid Ink</ComboBoxItem>
+    <ComboBoxItem>Hot Dog</ComboBoxItem>
+    <ComboBoxItem>Fish</ComboBoxItem>
+    <ComboBoxItem>Beetroot</ComboBoxItem>
+    <ComboBoxItem>Olive</ComboBoxItem>
+    <ComboBoxItem>Tomato</ComboBoxItem>
   </ComboBox>
 )
 
