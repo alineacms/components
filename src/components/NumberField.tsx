2954f96aae2dab46339786c366c2fed7d626389f
thibovi <thibovaningelgem@gmail.com>
2025-03-14 14:01:37 +0100

    Migrate NumberField component to new structure and enhance styling

---
+++ src/components/NumberField.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx'
+import {
+  Button,
+  Input,
+  NumberField as NumberFieldPrimitive,
+  type NumberFieldProps as NumberFieldPrimitiveProps
+} from 'react-aria-components'
+import {IcRoundKeyboardArrowDown} from '../icons/IcRoundKeyboardArrowDown.tsx'
+import {IcRoundKeyboardArrowUp} from '../icons/IcRoundKeyboardArrowUp.tsx'
+import {Label, type LabelSharedProps, labelProps} from './Label.tsx'
+import './NumberField.css'
+
+export interface NumberFieldProps
+  extends Omit<NumberFieldPrimitiveProps, 'children'>,
+    LabelSharedProps {
+  steppers?: boolean
+}
+
+export function NumberField({steppers = true, ...props}: NumberFieldProps) {
+  return (
+    <NumberFieldPrimitive
+      {...props}
+      className="alinea-rac-NumberField"
+      data-invalid={props.errorMessage ? true : undefined}
+    >
+      <Label {...labelProps(props)}>
+        <div
+          className={clsx('alinea-rac-NumberField-wrapper')}
+          data-invalid={props.errorMessage ? true : undefined}
+          data-steppers={steppers}
+        >
+          <Input className="alinea-rac-NumberField-input" />
+          {steppers && (
+            <div className="alinea-rac-NumberField-buttons">
+              <Button
+                className="alinea-rac-NumberField-button"
+                data-slot="increment"
+                slot="increment"
+              >
+                <IcRoundKeyboardArrowUp />
+              </Button>
+              <Button
+                className="alinea-rac-NumberField-button"
+                data-slot="decrement"
+                slot="decrement"
+              >
+                <IcRoundKeyboardArrowDown />
+              </Button>
+            </div>
+          )}
+        </div>
+      </Label>
+    </NumberFieldPrimitive>
+  )
+}
