040b56314f519ab35b0482743930a578a607d0e8
thibovi <thibovaningelgem@gmail.com>
2025-03-24 13:18:22 +0100

    refactor: streamline Menu integration in Toolbar stories

--- stories/Toolbar.stories.tsx
+++ stories/Toolbar.stories.tsx
@@ -1,94 +1,97 @@
-import {Menu, MenuItem, MenuTrigger} from 'react-aria-components'
 import {Button} from '../src/components/Button.tsx'
 import {
   ToolbarSeparator as Separator,
   Toolbar,
-  ToolbarGroup,
+  ToolbarGroup
 } from '../src/components/Toolbar.tsx'
 import {IcRoundBrightness} from '../src/icons/IcRoundBrightness.tsx'
-import {Popover} from '../src/todo/Popover.tsx'
 import '../src/components/Toolbar.css'
 import {Icon} from '../src/components/Icon.tsx'
-import { IcRoundUnfoldMore } from '../src/icons/IcRoundUnfoldMore.tsx'
+import {Menu, MenuItem} from '../src/components/Menu.tsx'
+import {IcRoundUnfoldMore} from '../src/icons/IcRoundUnfoldMore.tsx'
 
 export const Example = (args: any) => (
   <Toolbar aria-label="Text formatting" data-orientation="horizontal" {...args}>
     <ToolbarGroup>
-      <MenuTrigger>
-        <Button appearance='plain'>
-          Options...
-          <IcRoundUnfoldMore />
-        </Button>
-        <Popover>
-          <Menu>
-            <MenuItem>
-              <IcRoundBrightness />
-              Undo
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Redo
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Insert Link
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Insert Image
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Insert Grid
-            </MenuItem>
-          </Menu>
-        </Popover>
-      </MenuTrigger>
+      <Menu
+        label={
+          <Button appearance="plain">
+            Options...
+            <IcRoundUnfoldMore />
+          </Button>
+        }
+      >
+        <MenuItem>
+          <IcRoundBrightness />
+          Undo
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Redo
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Insert Link
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Insert Image
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Insert Grid
+        </MenuItem>
+      </Menu>
     </ToolbarGroup>
+
     <Separator />
+
     <ToolbarGroup>
       <Button size="square-petite" appearance="plain" data-appearance="active">
         <Icon icon={IcRoundBrightness} />
       </Button>
-      <MenuTrigger>
-      <Button size='large' appearance="plain">
-        <Icon icon={IcRoundBrightness}/> <Icon icon={IcRoundUnfoldMore} />
-      </Button>
-      <Popover>
-          <Menu>
-            <MenuItem>
-              <IcRoundBrightness />
-              Undo
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Redo
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Insert Link
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Insert Image
-            </MenuItem>
-            <MenuItem>
-              <IcRoundBrightness />
-              Insert Grid
-            </MenuItem>
-          </Menu>
-        </Popover>
-      </MenuTrigger>
-      
-        <Button  size="square-petite" appearance="plain">
+
+      <Menu
+        label={
+          <Button size="large" appearance="plain">
+            <Icon icon={IcRoundBrightness} />
+            <Icon icon={IcRoundUnfoldMore} />
+          </Button>
+        }
+      >
+        <MenuItem>
+          <IcRoundBrightness />
+          Undo
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Redo
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Insert Link
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Insert Image
+        </MenuItem>
+        <MenuItem>
+          <IcRoundBrightness />
+          Insert Grid
+        </MenuItem>
+      </Menu>
+
+      <Button size="square-petite" appearance="plain">
         <Icon icon={IcRoundBrightness} />
-        </Button>
+      </Button>
 
       <Button size="square-petite" appearance="plain">
         <Icon icon={IcRoundBrightness} />
       </Button>
     </ToolbarGroup>
+
     <Separator />
+
     <ToolbarGroup>
       <Button size="square-petite" appearance="plain">
         <Icon icon={IcRoundBrightness} />
@@ -97,13 +100,17 @@ export const Example = (args: any) => (
         <Icon icon={IcRoundBrightness} />
       </Button>
     </ToolbarGroup>
+
     <Separator />
+
     <ToolbarGroup>
       <Button size="square-petite" appearance="plain">
         <Icon icon={IcRoundBrightness} />
       </Button>
     </ToolbarGroup>
-    <Separator/>
+
+    <Separator />
+
     <ToolbarGroup>
       <Button size="square-petite" appearance="plain">
         <Icon icon={IcRoundBrightness} />
@@ -112,7 +119,9 @@ export const Example = (args: any) => (
         <Icon icon={IcRoundBrightness} />
       </Button>
     </ToolbarGroup>
-    <Separator/>
+
+    <Separator />
+
     <ToolbarGroup>
       <Button size="square-petite" appearance="plain">
         <Icon icon={IcRoundBrightness} />
