f0af4a3c1963b3dbf05867c4c23abb26494f0977
Ben Merckx <ben@codeurs.be>
2025-07-30 12:12:46 +0200

    Setup dashboard panes

--- src/stories/Dashboard.stories.tsx
+++ src/stories/Dashboard.stories.tsx
@@ -1,3 +1,52 @@
+import {Allotment} from 'allotment'
+import 'allotment/dist/style.css'
+import {Button} from '../components/Button.tsx'
+import {IcRoundAccountCircle} from './icons/IcRoundAccountCircle.tsx'
+import {IcRoundArchive} from './icons/IcRoundArchive.tsx'
+import {IcRoundHistory} from './icons/IcRoundHistory.tsx'
+import {IcRoundLanguage} from './icons/IcRoundLanguage.tsx'
+import {IcRoundSearch} from './icons/IcRoundSearch.tsx'
+import {IcRoundSettings} from './icons/IcRoundSettings.tsx'
+
 export function Home() {
-  return <div>hello</div>
+  return (
+    <Allotment>
+      <Allotment.Pane minSize={200} preferredSize={320}>
+        <div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 10
+            }}
+          >
+            <Button data-size="square-petite" appearance="active">
+              <IcRoundAccountCircle data-slot="icon" />
+            </Button>
+            <Button data-size="square-petite" appearance="plain">
+              <IcRoundArchive data-slot="icon" />
+            </Button>
+            <Button data-size="square-petite" appearance="plain">
+              <IcRoundHistory data-slot="icon" />
+            </Button>
+            <Button data-size="square-petite" appearance="plain">
+              <IcRoundLanguage data-slot="icon" />
+            </Button>
+            <Button data-size="square-petite" appearance="plain">
+              <IcRoundSearch data-slot="icon" />
+            </Button>
+            <Button data-size="square-petite" appearance="plain">
+              <IcRoundSettings data-slot="icon" />
+            </Button>
+          </div>
+        </div>
+      </Allotment.Pane>
+      <Allotment.Pane>
+        <div>B</div>
+      </Allotment.Pane>
+      <Allotment.Pane minSize={200} preferredSize={320}>
+        <div>C</div>
+      </Allotment.Pane>
+    </Allotment>
+  )
 }
