cad4f646a606d68fb6af1b062088ef768c6df273
Ben Merckx <ben@codeurs.be>
2025-07-30 15:18:34 +0200

    Update dashboard

--- src/stories/Dashboard.stories.tsx
+++ src/stories/Dashboard.stories.tsx
@@ -1,6 +1,8 @@
 import {Allotment} from 'allotment'
 import 'allotment/dist/style.css'
 import {Button} from '../components/Button.tsx'
+import {Example as TextExamples} from '../components/TextField.stories.tsx'
+import {DragNDrop} from '../components/Tree.stories.tsx'
 import {IcRoundAccountCircle} from './icons/IcRoundAccountCircle.tsx'
 import {IcRoundArchive} from './icons/IcRoundArchive.tsx'
 import {IcRoundHistory} from './icons/IcRoundHistory.tsx'
@@ -10,43 +12,58 @@ import {IcRoundSettings} from './icons/IcRoundSettings.tsx'
 
 export function Home() {
   return (
-    <Allotment>
-      <Allotment.Pane minSize={200} preferredSize={320}>
-        <div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 10
-            }}
-          >
-            <Button data-size="square-petite" appearance="active">
-              <IcRoundAccountCircle data-slot="icon" />
-            </Button>
-            <Button data-size="square-petite" appearance="plain">
-              <IcRoundArchive data-slot="icon" />
-            </Button>
-            <Button data-size="square-petite" appearance="plain">
-              <IcRoundHistory data-slot="icon" />
-            </Button>
-            <Button data-size="square-petite" appearance="plain">
-              <IcRoundLanguage data-slot="icon" />
-            </Button>
-            <Button data-size="square-petite" appearance="plain">
-              <IcRoundSearch data-slot="icon" />
-            </Button>
-            <Button data-size="square-petite" appearance="plain">
-              <IcRoundSettings data-slot="icon" />
-            </Button>
-          </div>
-        </div>
-      </Allotment.Pane>
-      <Allotment.Pane>
-        <div>B</div>
-      </Allotment.Pane>
-      <Allotment.Pane minSize={200} preferredSize={320}>
-        <div>C</div>
-      </Allotment.Pane>
-    </Allotment>
+    <div style={{display: 'flex', flexDirection: 'row', height: '100%'}}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 10
+        }}
+      >
+        <Button data-size="square-petite" appearance="active">
+          <IcRoundAccountCircle data-slot="icon" />
+        </Button>
+        <Button data-size="square-petite" appearance="plain">
+          <IcRoundArchive data-slot="icon" />
+        </Button>
+        <Button data-size="square-petite" appearance="plain">
+          <IcRoundHistory data-slot="icon" />
+        </Button>
+        <Button data-size="square-petite" appearance="plain">
+          <IcRoundLanguage data-slot="icon" />
+        </Button>
+        <Button data-size="square-petite" appearance="plain">
+          <IcRoundSearch data-slot="icon" />
+        </Button>
+        <Button data-size="square-petite" appearance="plain">
+          <IcRoundSettings data-slot="icon" />
+        </Button>
+      </div>
+
+      <div style={{flexGrow: 1, height: '100%'}}>
+        <Allotment>
+          <Allotment.Pane minSize={200} preferredSize={320} snap>
+            <div
+              style={{display: 'flex', flexDirection: 'column', height: '100%'}}
+            >
+              <div style={{flexGrow: 1, padding: '0 10px'}}>
+                <DragNDrop />
+              </div>
+              <div style={{marginLeft: 'auto'}}>
+                <Button>Create new</Button>
+              </div>
+            </div>
+          </Allotment.Pane>
+          <Allotment.Pane>
+            <div>
+              <TextExamples />
+            </div>
+          </Allotment.Pane>
+          {/*<Allotment.Pane minSize={200} preferredSize={320}>
+            <div>Preview</div>
+          </Allotment.Pane>*/}
+        </Allotment>
+      </div>
+    </div>
   )
 }
