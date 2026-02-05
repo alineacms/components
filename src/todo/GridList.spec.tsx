f086968beed773b492c541f63f8c0a4dbf07c6d6
Ben Merckx <ben@codeurs.be>
2026-02-05 15:15:47 +0100

    Interim

--- src/todo/GridList.spec.tsx
+++ src/todo/GridList.spec.tsx
@@ -1,53 +1,5 @@
 import {expect, test} from '@playwright/experimental-ct-react'
-import {useMemo} from 'react'
-import {useDragAndDrop} from 'react-aria-components'
-import {useListData} from 'react-stately'
-import {TextField} from '../components/TextField.tsx'
-import {GridList, GridListItem} from '../todo/GridList.tsx'
-
-interface Item {
-  key: string
-  name: string
-}
-
-function GridListWithInput() {
-  const initialItems = useMemo<Item[]>(
-    () =>
-      Array.from({length: 3}, (_, i) => ({
-        key: i.toString(),
-        name: `Item ${i + 1}`
-      })),
-    []
-  )
-  const list = useListData({
-    initialItems
-  })
-  const {dragAndDropHooks} = useDragAndDrop({
-    getItems: keys =>
-      [...keys].map(key => ({'text/plain': list.getItem(key)!.name})),
-    onReorder(e) {
-      if (e.target.dropPosition === 'before') {
-        list.moveBefore(e.target.key, e.keys)
-      } else if (e.target.dropPosition === 'after') {
-        list.moveAfter(e.target.key, e.keys)
-      }
-    }
-  })
-
-  return (
-    <GridList
-      aria-label="Reorderable list"
-      items={list.items}
-      dragAndDropHooks={dragAndDropHooks}
-    >
-      {item => (
-        <GridListItem id={item.key} textValue={item.name} header={item.name}>
-          <TextField label="Text field" placeholder="Type here..." />
-        </GridListItem>
-      )}
-    </GridList>
-  )
-}
+import {GridListWithInput} from './GridList.test.tsx'
 
 test.describe('GridList', () => {
   test('selecting text in input should not start drag', async ({
@@ -55,7 +7,7 @@ test.describe('GridList', () => {
     page
   }) => {
     const component = await mount(<GridListWithInput />)
-    const input = component.getByLabel('Text field')
+    const input = component.getByLabel('Text field').first()
     await input.click()
     await input.fill('Hello world')
 
---
+++ src/todo/GridList.test.tsx
@@ -0,0 +1,49 @@
+import {useMemo} from 'react'
+import {useDragAndDrop} from 'react-aria-components'
+import {useListData} from 'react-stately'
+import {TextField} from '../components/TextField.tsx'
+import {GridList, GridListItem} from './GridList.tsx'
+
+interface Item {
+  key: string
+  name: string
+}
+
+export function GridListWithInput() {
+  const initialItems = useMemo<Item[]>(
+    () =>
+      Array.from({length: 3}, (_, i) => ({
+        key: i.toString(),
+        name: `Item ${i + 1}`
+      })),
+    []
+  )
+  const list = useListData({
+    initialItems
+  })
+  const {dragAndDropHooks} = useDragAndDrop({
+    getItems: keys =>
+      [...keys].map(key => ({'text/plain': list.getItem(key)!.name})),
+    onReorder(e) {
+      if (e.target.dropPosition === 'before') {
+        list.moveBefore(e.target.key, e.keys)
+      } else if (e.target.dropPosition === 'after') {
+        list.moveAfter(e.target.key, e.keys)
+      }
+    }
+  })
+
+  return (
+    <GridList
+      aria-label="Reorderable list"
+      items={list.items}
+      dragAndDropHooks={dragAndDropHooks}
+    >
+      {item => (
+        <GridListItem id={item.key} textValue={item.name} header={item.name}>
+          <TextField label="Text field" placeholder="Type here..." />
+        </GridListItem>
+      )}
+    </GridList>
+  )
+}
